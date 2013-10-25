f3398f50f0891eb339cb5a83e530783e3379690f
Nathan Neri <nneri@mymail.mines.edu>
2013-10-25 16:25:07 -0600

    Added the skeleton for the panel setup.

--- src/ClueControlPanel.java
+++ src/ClueControlPanel.java
@@ -1,6 +1,14 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 
-
 public class ClueControlPanel extends JPanel {
-
+	public ClueControlPanel(){
+		SuggestionLogPanel log = new SuggestionLogPanel();
+		add(log, BorderLayout.CENTER); //so it's given the room it needs
+		
+		WhoseTurnPanel turn = new WhoseTurnPanel();
+		add(turn, BorderLayout.WEST);
+	}
+	
 }
