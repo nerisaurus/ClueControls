3a20cb2f9344b36043d07818bfbf9c5c27602406
Nathan Neri <nneri@mymail.mines.edu>
2013-10-25 16:13:36 -0600

    The very basic setup. We have a JFrame.

---
+++ src/ClueControlFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class ClueControlFrame extends JFrame{
+
+	public static void main(String[] args) {
+		ClueControlFrame gui = new ClueControlFrame();	
+		gui.setVisible(true);
+	}
+
+}
+
